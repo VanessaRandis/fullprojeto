6cc5143d463b1c1d98c75dc163aaf2e5a5f01fee
VanessaRandis <vanessa.randis@gmail.com>
2025-05-16 08:27:10 -0300

    altera banco de dados

--- src/main/java/com/front/ads/user/UserModel.java
+++ src/main/java/com/front/ads/user/UserModel.java
@@ -5,10 +5,12 @@ import java.util.UUID;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "user_model")
 public class UserModel {
 
     @Id
